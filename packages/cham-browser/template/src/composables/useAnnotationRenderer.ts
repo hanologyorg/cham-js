232e31bf00c9e2b478693e6f4a586c58955c7089
Ronald Tse <ronald.tse@ribose.com>
2026-06-01 17:48:06 +0800

    debug: show raw heshanggong-9 annotation data

--- packages/cham-browser/template/src/composables/useAnnotationRenderer.ts
+++ packages/cham-browser/template/src/composables/useAnnotationRenderer.ts
@@ -22,6 +22,7 @@ export function buildVerseAnnotations(annotations: Annotation[], verseIndex: num
 
   if (import.meta.env.SSR && anns.some(a => a.id === 'heshanggong-9')) {
     console.log('[DEBUG-BVA] verse', verseIndex, 'anns=', anns.map(a => `${a.id}[${a.range.start},${a.range.end}]`))
+    console.log('[DEBUG-BVA-RAW] all annotations count=', annotations.length, 'heshanggong-9 full=', JSON.stringify(annotations.find(a => a.id === 'heshanggong-9')))
   }
 
   const points = new Set<number>()
